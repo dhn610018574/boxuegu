51d80bf20d2e18b2f7c81499128ddd7fd8b02741
dhn <dhn610018574@sina.com>
2017-06-15 23:42:02 +0800

    02-login页面实现模块化

---
+++ public/js/index/login.js
@@ -0,0 +1,33 @@
+define(['jquery'], function ($) {
+
+    $('#loginForm').on('submit', function () {
+        // 进行用户输入校验
+        var tc_name = $('#tc_name').val();
+        var tc_pass = $('#tc_pass').val();
+        if (tc_name === '' || tc_pass === '') {
+            // 阻止表单提交！！！
+            return false;
+        }
+
+        // 作用：将表单序列化成一个 get请求的参数形式
+        var params = $(this).serialize();
+        var loginUrl = '/api/login?' + params;
+
+        // 发送ajax请求到接口
+        $.ajax({
+            url: loginUrl,
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+                if (data.code === 200) {
+                    // 跳转到首页
+                    location.href = '/';
+                }
+            }
+        });
+
+        // form 表单会自动提交，阻止默认提交
+        return false;
+    });
+})
